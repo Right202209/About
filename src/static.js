90faa29643da0a85503d0557e615760c77c954a4
Droit <111848491+Right202209@users.noreply.github.com>
2026-02-12 17:55:15 +0000

    feat: add ASCII name command and rewrite Pages workflow

--- src/static.js
+++ src/static.js
@@ -1,24 +1,40 @@
 import { contacts, skills, cartoons, books } from './info'
 
+const nameAsciiArt = [
+  'DDDD   RRRR   OOO    III  TTTTT',
+  'D   D  R   R O   O    I     T  ',
+  'D   D  RRRR  O   O    I     T  ',
+  'D   D  R R   O   O    I     T  ',
+  'DDDD   R  RR  OOO    III    T  '
+]
+
 export default {
-    contact: {
-      description: 'Return a list of my contact information.',
-      list: contacts.map(c => ({
-        type: 'info',
-        label: c.label,
-        content: c.content
-      }))
-    },
-    skill: {
-      description: 'Return a list of my skills and my rating of them.',
-      list: skills
-    },
-    cartoon: {
-      description: 'Return a list of cartoons that I have watched.',
-      list: cartoons
-    },
-    book: {
-      description: 'Return a list of books that I have not read, may I need read some of these.',
-      list: books
-    }
+  contact: {
+    description: 'Return a list of my contact information.',
+    list: contacts.map(c => ({
+      type: 'info',
+      label: c.label,
+      content: c.content
+    }))
+  },
+  skill: {
+    description: 'Return a list of my skills and my rating of them.',
+    list: skills
+  },
+  cartoon: {
+    description: 'Return a list of cartoons that I have watched.',
+    list: cartoons
+  },
+  book: {
+    description: 'Return a list of books that I have not read, may I need read some of these.',
+    list: books
+  },
+  ascii: {
+    description: 'Display large ASCII art of my name.',
+    list: nameAsciiArt
+  },
+  name: {
+    description: 'Display large ASCII art of my name.',
+    list: nameAsciiArt
   }
+}
